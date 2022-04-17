cf071fc3f58ee56195759d692e696528bc1c5db2
dungvu <dungvumta@gmail.com>
2022-04-17 22:56:42 +0700

    loading use css

---
+++ loading-screen-css/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from "react";
+import "./preLoader3.css";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, setloading] = useState(undefined);
+  const [completed, setcompleted] = useState(undefined);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setData(json);
+          setloading(true);
+          setTimeout(() => {
+            setcompleted(true);
+          }, 1000);
+        });
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      {!completed ? (
+        <>
+          {!loading ? (
+            <div className="spinner">
+              <span>Loading...</span>
+              <div className="half-spinner"></div>
+            </div>
+          ) : (
+            <div className="completed">&#x2713;</div>
+          )}
+        </>
+      ) : (
+        <>
+          <h1>Your Data</h1>
+        </>
+      )}
+    </>
+  );
+}
+
+export default App;
